78ef5f028392693f61b2a3366170c4eb38ca912c
hamm1 <mlhambright1@gmail.com>
2024-02-07 17:57:48 -0600

    ci

--- clean.py
+++ clean.py
@@ -11,13 +11,13 @@ def clean(path):
 if os.name == "posix":
   print("Linux or MacOS")
   remove = ["./node_modules","./dist","./server","./src-tauri/target",
-            "./bun.lockb","./package-lock.json","./tmp"]
+            "./bun.lockb","./package-lock.json","./tmp","./src-tauri/Cargo.lock"]
   for r in remove:
     clean(r)
 
 if os.name == "nt":
   print("Windows")
   remove = [".\\node_modules",".\\dist",".\\server",".\\src-tauri\\target",
-            ".\\bun.lockb",".\\package-lock.json",".\\tmp"]
+            ".\\bun.lockb",".\\package-lock.json",".\\tmp",".\\src-tauri\\Cargo.lock"]
   for r in remove:
     clean(r)
