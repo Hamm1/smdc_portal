33bd5ece4e1fea2d962f5955515c027def6038b8
hamm1 <mlhambright1@gmail.com>
2024-02-22 00:02:30 -0600

    updated deps

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,7 @@ mod environment;
 
 fn main() {
   tauri::Builder::default()
-    // .plugin(tauri_plugin_shell::init())
+    .plugin(tauri_plugin_shell::init())
     .invoke_handler(tauri::generate_handler![greet,environment::get_location_variables,environment::get_additional_variables,environment::get_additional_variables_remove,environment::get_user,environment::get_computer_name])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
