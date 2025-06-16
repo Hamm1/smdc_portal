6c09ca9203eae099897043b08a994c1726161d5f
hamm1 <mlhambright1@gmail.com>
2025-06-16 01:01:17 -0500

    updated

--- .ci/dagger/src/index.ts
+++ .ci/dagger/src/index.ts
@@ -67,4 +67,46 @@ export class Ci {
       .withExec(["bun", "tauri", "build", "--runner", "cargo-xwin", "--target", "x86_64-pc-windows-msvc"])
       .file("/app/src-tauri/target/x86_64-pc-windows-msvc/release/smdc_portal.exe")
   }
+
+  @func()
+  tests(src: Directory): File {
+    return dag
+      .container({ platform: "linux/amd64" as Platform })
+      .from("ubuntu:22.04")
+      .withExec(["apt-get", "update"])
+      .withExec(["apt-get", "install", "-y","unzip", "libdbus-1-dev", "pkg-config",
+         "zlib1g", "libevent-dev", "libwebkit2gtk-4.0-dev", "build-essential", "curl", "wget", 
+         "libssl-dev", "libgtk-3-dev", "libsoup-3.0-dev", "libjavascriptcoregtk-4.1-dev",
+         "librsvg2-dev", "libfuse2", "libappindicator3-dev", "libwebkit2gtk-4.1-dev", "libffi-dev", "patchelf"])
+      .withExec(["curl", "--proto", "=https", "--tlsv1.2", "-sSf","https://sh.rustup.rs", "-o", "rustup-init.sh"])
+      .withExec(["chmod", "+x", "rustup-init.sh"])
+      .withExec(["./rustup-init.sh", "-y", "--no-modify-path"])
+      .withEnvVariable("PATH", "/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin")
+      .withMountedDirectory("/app", src)
+      .withWorkdir("/app")
+      .withExec(["cargo", "test", "--manifest-path", "src-tauri/Cargo.toml"])
+  }
+
+  @func()
+  playwright(src: Directory): File {
+    return dag
+      .container({ platform: "linux/amd64" as Platform })
+      .from("ubuntu:22.04")
+      .withExec(["apt-get", "update"])
+      .withExec(["apt-get", "install", "-y","unzip", "curl",])
+      .withExec(["curl","-SLO","https://deb.nodesource.com/nsolid_setup_deb.sh"])
+      .withExec(["rm","-f","/usr/share/keyrings/nodesource.gpg"])
+      .withExec(["chmod","500","nsolid_setup_deb.sh"])
+      .withExec(["./nsolid_setup_deb.sh","23"])
+      .withExec(["apt-get","install","nodejs","-y"])
+      .withExec(["curl", "-fsSL", "https://bun.sh/install", "-o", "bun-installer.sh"])
+      .withExec(["chmod", "+x", "bun-installer.sh"])
+      .withExec(["./bun-installer.sh"])
+      .withEnvVariable("PATH", "/root/.bun/bin:/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin")
+      .withMountedDirectory("/app", src)
+      .withWorkdir("/app")
+      .withExec(["bun", "install"])
+      .withExec(["bun", "x", "playwright", "install", "--with-deps", "chromium"])
+      .withExec(["bun", "run", "test.e2e", "--reporter=line"])
+  }
 }
---
+++ justfile
@@ -0,0 +1,58 @@
+
+set shell := ["bash", "-uc"]
+set windows-shell := ["pwsh.exe","-c"]
+sudo := if os_family() == "windows" { "" } else { "sudo" }
+
+test:
+	cargo test --manifest-path src-tauri/Cargo.toml
+
+run:
+	bun run dev
+
+start:
+	bun run start
+
+install:
+	bun install || npm install
+
+build:
+	(bun install && bun run build:bun:debug) || (npm install -force && npm run build:npm:debug)
+
+release:
+	bun run build:bun || npm run build
+
+debug:
+	bun run build:bun:debug || npm run build:npm:debug
+
+format:
+	bun run fmt
+
+clean:
+	bun run clean || npm run clean
+
+upgrade:
+	bun update || npx npm-check-updates -u
+
+docker:
+	{{ sudo }} docker build -t tauri --output type=local,dest=./out/ .
+
+dagger := if os_family() == "windows" { "pwsh dagger.ps1" } else { "dagger.sh" }
+dagger:
+	cd .ci && {{ dagger }}
+
+flox:
+	({{ sudo }} docker run --pull always -v {{invocation_directory()}}:/smdc_portal -v {{invocation_directory()}}/.flox/build/zshrc:/root/.zshrc -v /var/run/docker.sock:/var/run/docker.sock --name=flox -d -it ghcr.io/flox/flox) || (echo "Container Exists")
+	({{ sudo }} docker start flox) || (echo "Container is already started...")
+	@{{ sudo }} docker exec -it -w /smdc_portal flox flox activate
+
+flox_delete:
+	{{ sudo }} docker rm -f flox
+
+code_server:
+	({{ sudo }} docker run -v /var/run/docker.sock:/var/run/docker.sock -v {{invocation_directory()}}:{{invocation_directory()}} --name=code_server -p 443:443 -d matthewhambright/code_server:latest) || (echo "Container Exists")
+
+code_server_use:
+	{{ sudo }} docker exec -it code_server zsh
+
+code_server_delete:
+	{{ sudo }} docker rm -f code_server
